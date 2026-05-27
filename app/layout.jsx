1eabf4b4383c318c72ce555545597d208ac6cba4
pnscoralie-jpg <pnscoralie@gmail.com>
2026-05-27 15:52:02 +0200

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Entretiens annuels 2025-2026",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
